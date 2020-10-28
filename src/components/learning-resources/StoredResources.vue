feb30606043d503a0e82283ed9e063ae5ac2c8fa
Yunus Emre Demirbulut <y.emre.demirbulut@gmail.com>
2020-10-28 14:36:33 +0300

    initial commit

---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <learning-resource 
+      v-for="resource in resources" 
+      :key="resource.id"
+      :id="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue';
+
+export default {
+  components: {
+    LearningResource
+  },
+  inject: ['resources']
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
